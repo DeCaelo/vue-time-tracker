130afd5f08d3251aa5a69969b0688eb955fca9e0
Decaelo <ludovic.cleuet@gmail.com>
2017-02-14 22:05:43 +0100

    end

--- src/main.js
+++ src/main.js
@@ -1,13 +1,41 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
+import Vue      from 'vue'
+import App      from './App'
+import Home     from './components/Home'
+import TimeEntries  from './components/TimeEntries'
+import LogTime    from './components/LogTime'
+
+import VueRouter  from 'vue-router'
+import VueResource  from 'vue-resource'
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: Home},
+    { path: '/home', component: Home},
+    { path: '/time-entries', component: TimeEntries,
+      children: [
+        {
+          path: 'log-time',
+          component: LogTime
+        }
+      ]
+    },
+    { path: '*', redirect: '/home'}
+  ],
+  mode: 'history' 
+})
+
+var bus = new Vue({});
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
+const app = new Vue({
   router,
-  template: '<App/>',
-  components: { App },
-});
+  data: {
+    bus: bus
+  },
+  render: h => h(App)
+}).$mount('#app')
